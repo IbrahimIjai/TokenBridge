b088e90a62785fabea80968c3857219ddef7fb9f
Sunday Ibrahim Ijai <ibrahimsundayijai@gmail.com>
2024-01-31 08:44:58 +0100

    Chore: Fix network switch

--- components/scaffold-eth/NetworkSwitch.tsx
+++ components/scaffold-eth/NetworkSwitch.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { useSwitchNetwork } from "wagmi";
+
+// import { useSwitchNetwork } from "wagmi";
 
 function NetworkSwitch() {
-  const { switchNetwork } = useSwitchNetwork();
-  return <div>
-    Fukkkk
-  </div>;
+  // const { switchNetwork } = useSwitchNetwork();
+  return (
+    <div>
+      {/* Fukkkk */}
+      ...
+    </div>
+  );
 }
 
 export default NetworkSwitch;
